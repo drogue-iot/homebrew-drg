51c5490767d2dfc31bd3e6846abe8473e244d957
Dejan Bosanac <dejan@sensatic.net>
2022-06-01 13:36:05 +0200

    Upgrade formula to 0.9.0

--- Formula/drg.rb
+++ Formula/drg.rb
@@ -1,8 +1,8 @@
 class Drg < Formula
   desc "A command line tool for managing apps and devices in Drogue IoT cloud"
   homepage "https://drogue.io"
-  url "https://github.com/drogue-iot/drg/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "43a8d1170d910ff93d344a0e161b23b834d854f055689ed2dbda0c1346f1a173"
+  url "https://github.com/drogue-iot/drg/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "da852e7870935c515d57b872a86aaec7ee4633c884d3c1a59156fe869c23f016"
   license "Apache-2.0"
 
   depends_on "rust" => :build
